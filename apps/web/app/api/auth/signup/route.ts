014a25d488a124e436d094f1ab0c304e521d4007
akash.kodegrid <akash.kodegrid@gmail.com>
2026-09-09 18:45:50 +0530

    added the signup, session api  functionality

---
+++ apps/web/app/api/auth/signup/route.ts
@@ -0,0 +1,98 @@
+import { prisma } from "@/lib/db";
+import { error } from "console";
+import { z } from "zod";
+import argon2 from "argon2";
+import { createHash, randomBytes } from "crypto";
+
+const singupSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+const SESSION_DURATION_MS = 1000 * 60 * 60 * 24 * 7; //7 days
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const result = singupSchema.safeParse(body);
+    if (!result.success) {
+      return Response.json(
+        {
+          error: "Invalid signup data",
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    const { email, password } = result.data;
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return Response.json(
+        {
+          error: "Unable to create account",
+        },
+        {
+          status: 409,
+        },
+      );
+    }
+
+    const passwordHash = await argon2.hash(password);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        passwordHash,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    });
+
+    const sessionToken = randomBytes(32).toString("hex");
+
+    const tokenHash = createHash("sha256").update(sessionToken).digest("hex");
+
+    const expiresAt = new Date(Date.now() + SESSION_DURATION_MS);
+
+    await prisma.session.create({
+      data: {
+        tokenHash,
+        userId: user.id,
+        expiresAt,
+      },
+    });
+
+    cookieStore.set({
+      name: "session",
+      value: sessionToken,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      expires: expiresAt,
+      path: "/",
+    });
+
+    return Response.json(
+      {
+        user,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return Response.json(
+      {
+        error: "Something went wrong",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
